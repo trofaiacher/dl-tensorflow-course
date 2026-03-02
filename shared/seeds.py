74dedf74cee6e74a2612228dea120b9ecc428030
trofaiacher <christian.hoelzl@gmail.com>
2026-03-02 11:01:15 +0100

    Create seeds.py

---
+++ shared/seeds.py
@@ -0,0 +1,10 @@
+import os
+import random
+import numpy as np
+import tensorflow as tf
+
+def set_seed(seed: int = 42):
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    tf.keras.utils.set_random_seed(seed)
