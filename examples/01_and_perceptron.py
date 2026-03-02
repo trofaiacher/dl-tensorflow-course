40e9ca3e6c3524ad5538b137f9b94e75a58338cf
trofaiacher <christian.hoelzl@gmail.com>
2026-03-02 11:18:57 +0100

    Delete examples/01_and_perceptron.py

--- examples/01_and_perceptron.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pathlib import Path
-
-# Add project root to Python path
-ROOT = Path(__file__).resolve().parents[2]
-sys.path.append(str(ROOT))
-
-from shared.seeds import set_seed
-from shared.plotting import savefig
