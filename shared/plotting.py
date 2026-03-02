3ef645a65bbe9efce16070bfa285da45367fefe7
trofaiacher <christian.hoelzl@gmail.com>
2026-03-02 11:28:27 +0100

    Update plotting.py

--- shared/plotting.py
+++ shared/plotting.py
@@ -1,11 +1,16 @@
 import matplotlib.pyplot as plt
 
-COLORS = {
-    "black":  "#000000",
-    "orange": "#E69F00",
-    "blue":   "#0072B2",
-    "red":    "#D55E00",
-}
+# -----------------------------
+# Colour-blind safe palette (Okabe–Ito)
+# -----------------------------
+COL_BLACK  = "#000000"
+COL_ORANGE = "#E69F00"
+COL_SKY    = "#56B4E9"
+COL_GREEN  = "#009E73"
+COL_BLUE   = "#0072B2"
+COL_RED    = "#D55E00"
+COL_PURPLE = "#CC79A7"
+COL_GREY   = "#7F7F7F"
 
 def savefig(name: str, dpi: int = 300):
     plt.savefig(name, dpi=dpi, bbox_inches="tight")
